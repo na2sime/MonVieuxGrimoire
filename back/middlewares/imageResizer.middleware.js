0581be3edc5a4f31d6d557573c27aec7b74a68ed
na2sime <nassime.abdiou@icloud.com>
2023-06-07 18:46:24 +0200

    finished

---
+++ back/middlewares/imageResizer.middleware.js
@@ -0,0 +1,31 @@
+const Jimp = require("jimp");
+
+const resizeImageMiddleware = async (req, res, next) => {
+    if (!req.file) {
+        return next();
+    }
+
+    const imagePath = req.file.path;
+
+    try {
+        const image = await Jimp.read(imagePath);
+        await image
+            .resize(Jimp.AUTO, 700) // Redimensionner l'image en 700px de hauteur et conserver le ratio
+            .quality(80) // Ajuster la qualité de l'image JPG (0-100)
+            .contain(500, Jimp.AUTO) // Redimensionner l'image en 500px de largeur et conserver le ratio
+            .writeAsync(imagePath); // Écrire l'image redimensionnée en écrasant l'ancienne image
+
+        console.log(
+            "Image convertie en format JPG et redimenssionnée"
+        );
+        next();
+    } catch (error) {
+        console.log("Une erreur est survenue lors du traitement de l'image !");
+        console.error(error);
+        res.status(500).json({
+            error: "Une erreur est survenue lors du traitement de l'image !",
+        });
+    }
+};
+
+module.exports = resizeImageMiddleware;
--- back/routes/book.routes.js
+++ back/routes/book.routes.js
@@ -4,12 +4,14 @@ const bookController = require("../controllers/book.controller");
 
 const multer = require("../middlewares/multer.middleware");
 const authMiddleware = require("../middlewares/auth.middleware");
+const corsMiddleware = require("../middlewares/cors.middleware");
+const imageResizer = require("../middlewares/imageResizer.middleware");
 
 router.get("/", bookController.getAllBooks);
 router.get("/bestrating", bookController.getTopBooks);
 router.get("/:id", bookController.getBookById);
-router.post("/", authMiddleware, multer, bookController.addBook);
-router.put("/:id", authMiddleware, multer, bookController.modifyBook);
+router.post("/", authMiddleware, multer, imageResizer, bookController.addBook);
+router.put("/:id", authMiddleware, multer, imageResizer, bookController.modifyBook);
 router.delete("/:id", authMiddleware, bookController.deleteBook);
 router.post('/:id/rating', authMiddleware, bookController.rateBook);
 
